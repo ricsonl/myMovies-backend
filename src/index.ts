04e3fb59cfcb3acc39178655234f530463476f32
Ricson Luiz <ricsonluizv@gmail.com>
2021-01-07 16:13:19 -0300

    pg back to latest version, remove cors origin and tsc

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://mymovies-frontend.herokuapp.com',
-  optionsSuccessStatus: 200
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
