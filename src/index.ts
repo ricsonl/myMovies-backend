f5c1288b3a3640247e248bfbd30f824ea548dc2d
Ricson Luiz <ricsonluizv@gmail.com>
2021-01-08 01:55:02 -0300

    cors config

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://mymovies-frontend.herokuapp.com',
+  optionsSuccessStatus: 200
+}));
 
 app.use(express.json());
 app.use(routes);
